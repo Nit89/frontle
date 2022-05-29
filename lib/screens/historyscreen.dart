66dc8bb92700d522bd92b9d4afa8552d26b73e36
Nit89 <nitesh382singh@gmail.com>
2022-05-30 04:16:08 +0530

    appbar text

--- lib/screens/historyscreen.dart
+++ lib/screens/historyscreen.dart
@@ -7,7 +7,7 @@ class History extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("data"),
+        title: const Text("History"),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
