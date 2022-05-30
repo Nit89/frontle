6bc2d741696997d6029123bdd8cc08eceed97f2d
Nit89 <nitesh382singh@gmail.com>
2022-05-30 16:40:11 +0530

    history box

--- lib/screens/historyscreen.dart
+++ lib/screens/historyscreen.dart
@@ -7,7 +7,10 @@ class History extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("History"),
+        title: const Text(
+          "History",
+          style: TextStyle(color: Colors.white),
+        ),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -22,52 +25,159 @@ class History extends StatelessWidget {
               child: Column(
                 children: [
                   Expanded(
-                    child: Column(
-                      children: [
-                        const Text('history'),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: const [
-                            Text("15"),
-                            Text("100"),
-                            Text("4"),
-                            Text("8")
-                          ],
-                        ),
-                        const Text("BADGES"),
-                        Expanded(
-                          child: Container(
-                            child: Padding(
-                              padding: const EdgeInsets.all(18.0),
-                              child: GridView.builder(
-                                  itemCount: 9,
-                                  physics: const NeverScrollableScrollPhysics(),
-                                  gridDelegate:
-                                      const SliverGridDelegateWithFixedCrossAxisCount(
-                                    crossAxisCount: 3,
-                                    // mainAxisSpacing: 4,
-                                    // crossAxisSpacing: 5
-                                  ),
-                                  itemBuilder: (context, index) {
-                                    return Padding(
-                                      padding: const EdgeInsets.all(18.0),
-                                      child: Container(
-                                        decoration: const BoxDecoration(
-                                          shape: BoxShape.circle,
-                                          color: Color(0xff2E8BF7),
-                                        ),
-                                        child: const Icon(
-                                          Icons.question_mark,
-                                          size: 50,
-                                          color: Color(0xff123563),
-                                        ),
-                                      ),
-                                    );
-                                  }),
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(''),
+                              const Text(
+                                'HISTORY',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w600),
+                              ),
+                              Image.asset(
+                                'assets/images/Subtract.png',
+                                fit: BoxFit.contain,
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            height: 3,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              //crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  children: [
+                                    Text(
+                                      "15",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w500,
+                                          fontSize: 15),
+                                    ),
+                                    SizedBox(height: 4),
+                                    Text(
+                                      'PLAYED',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w300,
+                                          fontSize: 12),
+                                    ),
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    Text(
+                                      "100%",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w500,
+                                          fontSize: 12),
+                                    ),
+                                    SizedBox(height: 4),
+                                    Text(
+                                      'WIN',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w300,
+                                          fontSize: 12),
+                                    )
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    Text(
+                                      "4",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w500,
+                                          fontSize: 12),
+                                    ),
+                                    SizedBox(height: 4),
+                                    Text('CURRENT\n STREAK',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w300,
+                                            fontSize: 12))
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    Text(
+                                      "8",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w500,
+                                          fontSize: 12),
+                                    ),
+                                    SizedBox(height: 4),
+                                    Text('MAX',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w300,
+                                            fontSize: 12)),
+                                    Text('STREAK',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w300,
+                                            fontSize: 12)),
+                                  ],
+                                )
+                              ],
                             ),
                           ),
-                        ),
-                      ],
+                          SizedBox(
+                            height: 5,
+                          ),
+                          const Text(
+                            "BADGES",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 15),
+                          ),
+                          Expanded(
+                            child: Container(
+                              child: Padding(
+                                padding: const EdgeInsets.all(18.0),
+                                child: GridView.builder(
+                                    itemCount: 9,
+                                    physics:
+                                        const NeverScrollableScrollPhysics(),
+                                    gridDelegate:
+                                        const SliverGridDelegateWithFixedCrossAxisCount(
+                                            crossAxisCount: 3,
+                                            mainAxisSpacing: 4,
+                                            crossAxisSpacing: 5),
+                                    itemBuilder: (context, index) {
+                                      return Padding(
+                                        padding: const EdgeInsets.all(10.0),
+                                        child: Container(
+                                          decoration: const BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            color: Color(0xff2E8BF7),
+                                          ),
+                                          child: Image.asset(
+                                            'assets/images/badge1.png',
+                                            fit: BoxFit.contain,
+                                          ),
+                                        ),
+                                      );
+                                    }),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   )
                 ],
