4af9aaf85994b5a7e60dde8875c7f5fb8d1c5e33
Nit89 <nitesh382singh@gmail.com>
2022-05-30 17:24:18 +0530

    historyscreen completed

--- lib/screens/historyscreen.dart
+++ lib/screens/historyscreen.dart
@@ -7,14 +7,114 @@ class History extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          "History",
-          style: TextStyle(color: Colors.white),
-        ),
+        backgroundColor: Color(0xff123563),
+        title: Text('FRONTLYNE'),
+        leading: Icon(Icons.keyboard_arrow_left_outlined),
+        centerTitle: true,
+        elevation: 0,
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      "80",
+                      style: TextStyle(
+                          fontSize: 14.9, fontWeight: FontWeight.w500),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Image.asset(
+                      'assets/images/coins.png',
+                      width: 17,
+                      height: 17,
+                    ),
+                  ],
+                ),
+                Text(
+                  "FRONTLE",
+                  style: TextStyle(
+                      fontSize: 27,
+                      color: Color(0xff123563),
+                      fontWeight: FontWeight.w500),
+                ),
+                Icon(
+                  Icons.info_outline_rounded,
+                  color: Color(0xff707070),
+                  size: 22,
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: 42,
+                  height: 42,
+                  decoration: BoxDecoration(
+                    color: Color(0xff123563),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(8),
+                    ),
+                  ),
+                  child: Image.asset(
+                    'assets/images/bar.png',
+                    fit: BoxFit.scaleDown,
+                  ),
+                ),
+                Column(
+                  children: [
+                    Container(
+                      width: 42,
+                      height: 42,
+                      decoration: BoxDecoration(
+                        color: Color(0xff123563),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(8),
+                        ),
+                      ),
+                      child: Image.asset(
+                        'assets/images/bulb.png',
+                        fit: BoxFit.scaleDown,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Row(
+                      children: [
+                        Text("10"),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Image.asset(
+                          'assets/images/coins.png',
+                          width: 10,
+                          height: 10,
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 8,
+          ),
           Center(
             child: Container(
               width: 342,
@@ -142,7 +242,7 @@ class History extends StatelessWidget {
                             "BADGES",
                             style: TextStyle(
                                 color: Colors.white,
-                                fontWeight: FontWeight.w600,
+                                fontWeight: FontWeight.w500,
                                 fontSize: 15),
                           ),
                           Expanded(
@@ -168,7 +268,7 @@ class History extends StatelessWidget {
                                           ),
                                           child: Image.asset(
                                             'assets/images/badge1.png',
-                                            fit: BoxFit.contain,
+                                            fit: BoxFit.scaleDown,
                                           ),
                                         ),
                                       );
@@ -176,6 +276,39 @@ class History extends StatelessWidget {
                               ),
                             ),
                           ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Padding(
+                                padding:
+                                    EdgeInsets.only(right: 15.0, bottom: 23),
+                                child: Container(
+                                    width: 74,
+                                    height: 27,
+                                    decoration: BoxDecoration(
+                                        color: Color(0xff9FE587),
+                                        borderRadius: BorderRadius.circular(7)),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Icon(
+                                          Icons.share,
+                                          size: 13,
+                                          color: Color(0xff123563),
+                                        ),
+                                        Text(
+                                          'SHARE',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.w600,
+                                              fontSize: 12,
+                                              color: Color(0xff123563)),
+                                        ),
+                                      ],
+                                    )),
+                              ),
+                            ],
+                          )
                         ],
                       ),
                     ),
