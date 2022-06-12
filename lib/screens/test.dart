774f23b9f5293c745b6eaa9825bd2f1631be305b
Nit89 <nitesh382singh@gmail.com>
2022-06-12 20:37:47 +0530

    test

--- lib/screens/test.dart
+++ lib/screens/test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontle/screens/screen1.dart';
+import 'package:frontle/utils/dimension.dart';
 
 class TEST extends StatefulWidget {
   TEST({Key? key}) : super(key: key);
@@ -11,16 +12,139 @@ class TEST extends StatefulWidget {
 class _TESTState extends State<TEST> {
   @override
   Widget build(BuildContext context) {
+    print("current height ${MediaQuery.of(context).size.height}");
     return Scaffold(
       appBar: AppBar(
-        title: GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => FrontleScreen()),
-              );
-            },
-            child: Text('nitesh')),
+        leading: Icon(
+          Icons.keyboard_arrow_left_outlined,
+          size: Dimension.height30,
+        ),
+        backgroundColor: const Color(0xff123563),
+        title: const Text('FRONTLYNE'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(Dimension.height20),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'CONTENT',
+                  style: TextStyle(
+                      fontSize: Dimension.height18,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xff2E8BF7)),
+                ),
+                Icon(
+                  Icons.info_outline_rounded,
+                  color: Color(0xff707070),
+                )
+              ],
+            ),
+            SizedBox(
+              height: Dimension.height35,
+            ),
+            Container(
+              width: Dimension.Fyiconatinerwidth,
+              height: Dimension.Fyiconatinerheight,
+              decoration: BoxDecoration(
+                color: const Color(0xff123563),
+                border: Border.all(
+                  color: Colors.black,
+                ),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(Dimension.height16),
+                ),
+              ),
+              child: Padding(
+                padding: EdgeInsets.only(
+                    top: Dimension.height20, left: Dimension.height15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      "   FYI",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: Dimension.height17,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(Dimension.height3),
+                      child: SizedBox(
+                        height: Dimension.height50,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: Color(0xff2E8BF7),
+                              radius: Dimension.radius30,
+                              child: CircleAvatar(
+                                radius: Dimension.radius22,
+                                backgroundColor: Colors.white,
+                              ),
+                            ),
+                            SizedBox(
+                              width: Dimension.height30,
+                            ),
+                            CircleAvatar(
+                              backgroundColor: Color(0xff2E8BF7),
+                              radius: Dimension.radius30,
+                              child: CircleAvatar(
+                                radius: Dimension.radius22,
+                                backgroundColor: Colors.white,
+                              ),
+                            ),
+                            SizedBox(
+                              width: Dimension.height30,
+                            ),
+                            CircleAvatar(
+                              backgroundColor: Color(0xff2E8BF7),
+                              radius: Dimension.radius30,
+                              child: CircleAvatar(
+                                radius: Dimension.radius22,
+                                backgroundColor: Colors.white,
+                              ),
+                            ),
+                            SizedBox(
+                              width: Dimension.height30,
+                            ),
+                            CircleAvatar(
+                              backgroundColor: Color(0xff2E8BF7),
+                              radius: Dimension.radius30,
+                              child: CircleAvatar(
+                                radius: Dimension.radius22,
+                                backgroundColor: Colors.white,
+                              ),
+                            ),
+                            SizedBox(
+                              width: Dimension.height30,
+                            ),
+                            CircleAvatar(
+                              backgroundColor: Color(0xff2E8BF7),
+                              radius: Dimension.radius30,
+                              child: CircleAvatar(
+                                radius: Dimension.radius22,
+                                backgroundColor: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: Dimension.height23,
+            ),
+          ],
+        ),
       ),
     );
   }
